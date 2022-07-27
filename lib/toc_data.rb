682259c36e17c73469e6626e856fb4a0a2dafc19
Dmitry Korneev <st.lynx@gmail.com>
2022-07-27 11:20:06 +0300

    unique headers based on a whole page content

--- lib/toc_data.rb
+++ lib/toc_data.rb
@@ -2,18 +2,22 @@ require 'nokogiri'
 
 def toc_data(page_content)
   html_doc = Nokogiri::HTML::DocumentFragment.parse(page_content)
-
+  header_ids = {}
   # get a flat list of headers
-  headers = []
-  html_doc.css('h1, h2, h3').each do |header|
-    headers.push({
-      id: header.attribute('id').to_s,
-      content: header.children,
-      title: header.children.to_s.gsub(/<[^>]*>/, ''),
-      level: header.name[1].to_i,
-      children: []
-    })
-  end
+  headers =
+    html_doc.css('h1, h2, h3').each_with_object([]) do |header, result|
+      orig_id = header['id'] || 'header-id'
+      header_ids[orig_id] ||= 0
+      header_ids[orig_id] += 1
+      header['id'] = "#{orig_id}-#{header_ids[orig_id]}" if header_ids[orig_id] > 1
+      result.push({
+        id: header['id'],
+        content: header.children,
+        title: header.children.to_s.gsub(/<[^>]*>/, ''),
+        level: header.name[1].to_i,
+        children: []
+      })
+    end
 
   [3,2].each do |header_level|
     header_to_nest = nil
@@ -27,5 +31,6 @@ def toc_data(page_content)
       end
     end
   end
-  headers
+
+  [headers, html_doc.to_html]
 end
